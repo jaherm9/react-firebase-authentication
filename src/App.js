89c6ed930625a9182d835842f63a3e80c9166930
shaka <jaherm65@gmail.com>
2021-10-14 09:14:00 +0600

    module 57-1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import './App.css';
 import InitializeAuthentication from './Firebase/firebase.init';
 
 
 InitializeAuthentication();
-
+const googleProvider = new GoogleAuthProvider();
 function App() {
+  const auth = getAuth();
   const handleGoogleSignIn = () =>{
-
+    signInWithPopup(auth, googleProvider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
   }
   return (
     <div className="App">
