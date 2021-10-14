bbfd6cf314111c452016a57e453c54780b28e26d
shaka <jaherm65@gmail.com>
2021-10-14 09:57:48 +0600

    57-3 Use Bootstrap From And Get User Email And Password

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
 import InitializeAuthentication from './Firebase/firebase.init';
 
@@ -6,6 +7,9 @@ import InitializeAuthentication from './Firebase/firebase.init';
 InitializeAuthentication();
 const googleProvider = new GoogleAuthProvider();
 function App() {
+  const [email, setEmail] = useState('');
+  const [ password, setPassword] = useState('');
+
   const auth = getAuth();
   const handleGoogleSignIn = () =>{
     signInWithPopup(auth, googleProvider)
@@ -14,26 +18,47 @@ function App() {
       console.log(user);
     })
   }
+
+const handleEmailChange = e =>{
+setEmail(e.target.value);
+}
+
+const handlePasswordChange = e =>{
+  setPassword(e.target.value)
+}
 const handleRegistration = e => {
-console.log('registration will be added')
+console.log(email, password);
 e.preventDefault();
 }
 
   return (
-    <div className="App">
+    <div className="mx-5">
       <form onSubmit={handleRegistration}>
-        <h3>Please Register</h3>
-        <label htmlFor="email">Email:</label>
-        <input type="text" name="email"/>
-        <br />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="" />
-        <br />
-        <input type="submit" value="Register" />
-      </form>
-      <br /><br /><br />
-      <div>--------------------------------------</div>
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        <h3 className="text-primary">Please Register</h3>
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input onBlur={handleEmailChange} type="email" class="form-control" id="inputEmail3"/>
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input onBlur={handlePasswordChange} type="password" class="form-control" id="inputPassword3"/>
+    </div>
+  </div>
+  <div class="row mb-3">
+    <div class="col-sm-10 offset-sm-2">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck1"/>
+        <label class="form-check-label" for="gridCheck1">
+          Example checkbox
+        </label>
+      </div>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
     </div>
   );
 }
