2f9db25640f0a2611a6e68fcc9482fb017910592
shaka <jaherm65@gmail.com>
2021-10-14 09:07:10 +0600

    ready development environment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import InitializeAuthentication from './Firebase/firebase.init';
+
+
+InitializeAuthentication();
 
 function App() {
+  const handleGoogleSignIn = () =>{
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCjgw0-YYL6C0dniWX-CGJ6vnrkZ3THDfE",
+    authDomain: "react-firebase-authentic-73a53.firebaseapp.com",
+    projectId: "react-firebase-authentic-73a53",
+    storageBucket: "react-firebase-authentic-73a53.appspot.com",
+    messagingSenderId: "238031665631",
+    appId: "1:238031665631:web:225661310d62857cb02c6a"
+  };
+export default firebaseConfig;
